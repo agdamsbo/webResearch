cef8c1120788e1806b84235f41709566b604f9d6
Andreas Gammelgaard Damsbo <andreas@gdamsbo.dk>
2024-12-19 11:32:32 +0100

    experimenting with a default theme

--- R/theme.R
+++ R/theme.R
@@ -4,26 +4,32 @@
 #'
 #' @returns theme list
 #' @export
-custom_theme <- function(...){
+custom_theme <- function(...,
+                         version = 5,
+                         primary = "#1E4A8F",
+                         secondary = "#FF6F61",
+                         # success = "#1E4A8F",
+                         # info = ,
+                         # warning = ,
+                         # danger = ,
+                         # fg = "#000",
+                         # bg="#fff",
+                         bootswatch = "united",
+                         base_font = bslib::font_google("Montserrat"),
+                         # base_font = bslib::font_google("Alice"),
+                         # heading_font = bslib::font_google("Jost", wght = "800"),
+                         # heading_font = bslib::font_google("Noto Serif"),
+                         # heading_font = bslib::font_google("Alice"),
+                         heading_font = bslib::font_google("Public Sans",wght = "700"),
+                         code_font = bslib::font_google("Open Sans")){
   bslib::bs_theme(
     ...,
-    # preset = "united",
-    version = 5,
-    primary = "#1E4A8F",
-    secondary = "#FF6F61",
-    # success = "#1E4A8F",
-    # info = ,
-    # warning = ,
-    # danger = ,
-    # fg = "#000",
-    # bg="#fff",
-    bootswatch = "united",
-    base_font = bslib::font_google("Montserrat"),
-    # base_font = bslib::font_google("Alice"),
-    # heading_font = bslib::font_google("Jost", wght = "800"),
-    # heading_font = bslib::font_google("Noto Serif"),
-    # heading_font = bslib::font_google("Alice"),
-    heading_font = bslib::font_google("Public Sans",wght = "700"),
-    code_font = bslib::font_google("Open Sans")
+    version = version,
+    primary = primary,
+    secondary = secondary,
+    bootswatch = bootswatch,
+    base_font = base_font,
+    heading_font = heading_font,
+    code_font = code_font
   )
 }
